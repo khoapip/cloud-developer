cb33f424da8be5bfe7cbcaf4164a2cf23a9d775d
khoapip <ngotheanhkhoa@gmail.com>
2023-11-10 01:58:54 +0700

    submit

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,7 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-um08jws6h2ouxw13.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,10 +58,23 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const response = await Axios(jwksUrl)
+  const signingKey = response.data['keys'].find(
+    key => key['kid'] === jwt['header']['kid']
+  )
+  if (!signingKey) {
+    throw new Error(`Unable to find a signing key`)
+  }
+  const pem = transformCertToPem(signingKey.x5c[0]);
+  return verify(token, pem, {
+    algorithms: ['RS256']
+  }) as JwtPayload
+}
+
+function transformCertToPem(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  const pem = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return pem;
 }
 
 function getToken(authHeader: string): string {
